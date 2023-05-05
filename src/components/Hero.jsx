970a9c468aebb4c0f4ce72c70fa92f3b43948ee0
FukurouHaiRiyo <andreipanait00@gmail.com>
2023-05-05 20:41:25 +0300

    updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,13 @@ const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-        <img src={logo} alt='summy_logo' className='w-28 object-contain'/>
+        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg hover:text-gray-600">
+            <li>
+              <a href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500 hover:text-gray-600" aria-current="page">Home</a>
+            </li>
+          </ul>
+        </div>
 
         <button type='button' onClick={() => window.open('')} className='black_btn'>
           GitHub
